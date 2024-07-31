6fa2b8a0301dd301466eb9ce5c6a0ddd1431d0fe
karthik-yml <karthik.tv@ymedialabs.com>
2024-07-31 16:27:05 +0530

    added fix for env

--- auth/jwt_utils.py
+++ auth/jwt_utils.py
@@ -4,8 +4,8 @@ from typing import Optional
 
 from jose import JWTError, jwt
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-ALGORITHM = os.environ.get('ALGORITHM')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
+ALGORITHM = os.environ.get('ALGORITHM', 'HS256')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
